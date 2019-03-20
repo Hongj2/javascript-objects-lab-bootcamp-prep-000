1b25c0a1fed504bbee385e2203dc39d4f0d1f1df
Jessica Hong <jessicahong.24@gmail.com>
2019-03-20 18:18:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var recipes = {
   prop: 1
 }
@@ -18,3 +19,14 @@ function destructivelyDeleteFromObjectByKey(object, key) {
   delete object.prop
   return object
 }
+=======
+var recipes = Object({})
+
+function updateObjectWithKeyAndValue (object,key, value){
+  
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  
+}
+function 
+>>>>>>> 9ec14a79c07906dc80a31c0b10f4663ef938d4cd
